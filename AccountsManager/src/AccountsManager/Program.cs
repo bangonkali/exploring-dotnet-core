29204b88d81a6ef2845dd78ff7c94bc5f660704a
gilmichael@acomsys.net <58149qnjd2YE>
2016-10-26 02:59:17 +0800

    Using Visual Studio 2015

--- AccountsManager/src/AccountsManager/Program.cs
+++ AccountsManager/src/AccountsManager/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication
         {
             MySqlConnection connection = new MySqlConnection
             {
-                ConnectionString = "sslmode=none;server=localhost;user id=user;password=password;persistsecurityinfo=True;port=3306;database=hwapp"
+                ConnectionString = "sslmode=none;server=192.168.254.100;user id=user;password=password;persistsecurityinfo=True;port=3306;database=hwapp"
             };
 
             connection.Open();
