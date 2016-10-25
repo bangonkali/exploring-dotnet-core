19cb66680d61238e75989b7e60a21103873ba0d5
gilmichael <gilmichael@acomsys.net>
2016-10-26 02:18:48 +0800

    Init.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MySqlConnection connection = new MySqlConnection
+            {
+                ConnectionString = "sslmode=none;server=localhost;user id=user;password=password;persistsecurityinfo=True;port=3306;database=hwapp"
+            };
+
+            connection.Open();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM users;", connection);
+
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                System.Console.WriteLine("username");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.FieldCount}");
+                    string row = $"{reader["username"]}";
+                    System.Console.WriteLine(row);
+                }
+            }
+
+            connection.Close();
+        }
+    }
+}
